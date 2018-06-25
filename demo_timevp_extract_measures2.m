ffd8cb89e4fd15dab0054775458998b829b652ea
chenyu18 <chenyu@indiana.edu>
2018-06-25 13:42:24 -0400

    Add files via upload

---
+++ demo_timevp_extract_measures2.m
@@ -0,0 +1,14 @@
+clear;
+addpath('timevp_lib'); 
+
+%%
+subject_list = [7002 7003 7005 7006];
+variable_list = {'cevent_eye_roi_child'};
+segment_event = 'cevent_speech_naming_local-id';
+stats_args.whence = 'start';
+stats_args.interval = [-3 0];
+save_filename = 'example_extract_measures2.csv';
+
+%%
+timevp_extract_measures(subject_list, variable_list, segment_event, save_filename, stats_args);
+
---
+++ demo_timevp_extract_measures3.m
@@ -0,0 +1,18 @@
+clear; 
+addpath('timevp_lib')
+
+%%
+subject_list = [7002 7003 7005 7006];
+variable_list = {'cevent_eye_roi_child'};
+segment_event = 'cevent_speech_naming_local-id';
+num_categories = 3; 
+args.event_category = 1:num_categories;
+args.var_category = 1:num_categories;
+args.groupid_matrix = [1 2 2; 
+                       2 1 2;
+                       2 2 1];
+args.group_label = {'target', 'distractor'};
+save_filename = 'example_extract_measures3.csv';
+
+%%
+timevp_extract_measures(subject_list, variable_list, segment_event, save_filename, args);
