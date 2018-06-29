8694feda679b6e2b89e07661eb661dc7054a11a1
Tian Linger Xu <linger.xt@gmail.com>
2018-06-29 00:37:08 -0400

    update demo files

--- demo_timevp_extract_pairs2.m
+++ demo_timevp_extract_pairs2.m
@@ -4,8 +4,8 @@ addpath('timevp_lib');
 variable1 = 'cevent_eye_roi_child';
 variable2 = 'cevent_eye_roi_parent';
 subject_list = [1203 1205 1206 1208];
-timing_relation = 'more(on1, off2 0.3) & less(off1, on2, 1)';
+timing_relation = 'more(on1, off2, 0.3) & less(off1, on2, 1)';
 %mapping = [(1:25)' (1:25)'];
 
-dir_savefiles = 'timevp_output_files/yulab_analysis';
+dir_savefiles = 'timevp_output_files';
 timevp_extract_pairs_by_subject(subject_list, variable1, variable2, timing_relation, dir_savefiles);
